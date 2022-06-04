a4f8b9bef4260b8c85b45cb8d33b385be8d91a73
AjayaRamachandran <ajaya.ramachandran@gmail.com>
2022-06-04 19:44:28 -0400

    made some progress

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 #------Import------
 
+from tracemalloc import start
+from turtle import bgcolor
 from litemapy import Schematic, Region, BlockState
 import random
 import structuregeneration as sg
+import tkinter as tk
+import time
+from tkinter.filedialog import asksaveasfile
+
 
 #------Setup------
 
@@ -35,17 +41,56 @@ def preview():
     reg = list(schem.regions.values())[0]
 
     
-    for y in reg.yrange():
-        for x in reg.xrange():
-            b = reg.getblock(x, 20-y, 10)
-            if b.blockid == "minecraft:air":
-                print(" ", end="")
-            else:
-                print("#", end='')
-        print()
+    for z in reg.zrange():
+        for y in reg.yrange():
+            for x in reg.xrange():
+                b = reg.getblock(x, 20-y, z)
+                if b.blockid == "minecraft:air":
+                    print(" ", end="")
+                else:
+                    print("#", end='')
+            print()
 
 
 #------Main------
+root = tk.Tk()
+root.geometry("500x500")
+root.title("Schemedit v1.0.1")
+
+
+openingFrame = tk.Frame(root, bg="black")
+openingFrame.grid(padx=200,pady=200)
+
+
+
+def openPreview():
+    root2 = tk.Tk()
+    root2.geometry("200x100")
+    root2.title("Rendering House")
+
+    renderFrame = tk.Frame(root2, bg="blue")
+    renderFrame.grid(padx=50,pady=50)
+
+    renderText = tk.Label(renderFrame, text="Creating House...")
+    renderText.pack()
+
+    name = asksaveasfile(initialfile = 'Creation.litematic', mode='w',defaultextension=".litematic", filetypes=[("Litematica Schematics","*.litematic")])
+    text2save = str(open("creation.litematic"))
+    name.write(text2save)
+    name.close
+
+#renderFrame.tkraise()
+openingFrame.tkraise()
+
+startText = tk.Label(openingFrame, text="Schemedit v1.0.1")
+startText.pack()
+
+startButton = tk.Button(openingFrame, text="Generate House", command=openPreview)
+startButton.pack()
+
+
+
+
 
 genStruct()
 
@@ -55,3 +100,7 @@ schem.save("creation.litematic")
 
 
 preview()
+
+
+#------Mainloop------
+root.mainloop()

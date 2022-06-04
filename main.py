13aa21bb1dee50301d6fc9382c32fbbe4db59fdd
AjayaRamachandran <ajaya.ramachandran@gmail.com>
2022-06-03 23:22:34 -0400

    worked on turtle

--- main.py
+++ main.py
@@ -1,36 +1,73 @@
+#------Import------
+
 from litemapy import Schematic, Region, BlockState
 import random
+import turtle as trtl
+
+#------Setup------
 
 # Shortcut to create a schematic with a single region
-reg = Region(0, 0, 0, 21, 21, 21)
+reg = Region(0, 0, 0, 100, 100, 100)
 schem = reg.as_schematic(name="Creation1", author="Anonymous", description="Made with Schematic Editor, Powered by Litemapy")
 
 # Create the block state we are going to use
 block = BlockState("minecraft:light_blue_concrete")
 
-# Build the planet
-"""
-for x, y, z in reg.allblockpos():
-    if round(((x-10)**2 + (y-10)**2 + (z-10)**2)**.5) <= 10:
-        reg.setblock(x, y, z, block)
-"""
-for x, y, z in reg.allblockpos():
-    if random.randint(0,1) == 1:
-        reg.setblock(x, y, z, block)
+
+previewScreen = trtl.Screen()
+previewScreen.setup(500,500)
+previewScreen.title('Schematic Preview')
+
+drawer = trtl.Turtle()
+drawer.hideturtle()
+drawer.goto(-100, 0)
+
+#------Functions------
+
+# Build the schematic
+def genStruct():
+    for x, y, z in reg.allblockpos():
+        if random.randint(0,1) == 1:
+            reg.setblock(x, y, z, block)
+
+
+# Preview the schematic
+def preview():
+
+    # Load the schematic and get its first region
+    schem = Schematic.load("creation.litematic")
+    reg = list(schem.regions.values())[0]
+
+
+    for x in reg.xrange():
+        drawer.goto(drawer.xcor() += 1, drawer.ycor())
+        for y in reg.zrange():
+            drawer.goto()
+            for z in reg.yrange():
+                b = reg.getblock(x, y, z)
+                if b.blockid == "minecraft:air":
+                    drawer.pendown()
+                    drawer.penup()
+
+    for x in reg.xrange():
+        for z in reg.zrange():
+            b = reg.getblock(x, 10, z)
+            if b.blockid == "minecraft:air":
+                print(" ", end="")
+            else:
+                print("#", end='')
+        print()
+
+
+#------Main------
+
+genStruct()
+
 
 # Save the schematic
 schem.save("creation.litematic")
 
-# Load the schematic and get its first region
-schem = Schematic.load("creation.litematic")
-reg = list(schem.regions.values())[0]
 
-# Print out the basic shape
-for x in reg.xrange():
-    for z in reg.zrange():
-        b = reg.getblock(x, 10, z)
-        if b.blockid == "minecraft:air":
-            print(" ", end="")
-        else:
-            print("#", end='')
-    print()
+preview()
+
+previewScreen.mainloop()

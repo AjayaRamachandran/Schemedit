bc82f196b4d749377042dd1df19e03011f80b79e
AjayaRamachandran <ajaya.ramachandran@gmail.com>
2022-06-03 22:08:36 -0400

    worked on litematica a bit

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
-# Import Statements
-import turtle as trtl
 from litemapy import Schematic, Region, BlockState
+import random
 
-# Setup
-volumeData = []
+# Shortcut to create a schematic with a single region
+reg = Region(0, 0, 0, 21, 21, 21)
+schem = reg.as_schematic(name="Creation1", author="Anonymous", description="Made with Schematic Editor, Powered by Litemapy")
 
+# Create the block state we are going to use
+block = BlockState("minecraft:light_blue_concrete")
 
-for i in range(1000):
-    volumeData.append("0")
+# Build the planet
+"""
+for x, y, z in reg.allblockpos():
+    if round(((x-10)**2 + (y-10)**2 + (z-10)**2)**.5) <= 10:
+        reg.setblock(x, y, z, block)
+"""
+for x, y, z in reg.allblockpos():
+    if random.randint(0,1) == 1:
+        reg.setblock(x, y, z, block)
+
+# Save the schematic
+schem.save("creation.litematic")
+
+# Load the schematic and get its first region
+schem = Schematic.load("creation.litematic")
+reg = list(schem.regions.values())[0]
+
+# Print out the basic shape
+for x in reg.xrange():
+    for z in reg.zrange():
+        b = reg.getblock(x, 10, z)
+        if b.blockid == "minecraft:air":
+            print(" ", end="")
+        else:
+            print("#", end='')
+    print()
